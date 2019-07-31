88a53d35ef1461300e39e4b255bf3958d5060815
felipech <felipech@outlook.com>
2019-07-31 00:37:50 -0700

    Init Material ui with theming

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from '../resources/logo.svg';
-import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { brown } from '@material-ui/core/colors';
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  createMuiTheme,
+  CssBaseline
+} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: brown
+  }
+});
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Typography variant='h6' color='inherit'>
+            Main
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </MuiThemeProvider>
   );
 };
 
